98f7c270e0251872bc826c65ab168e9c149026b2
nimrod njau <nimrodnjau@gmail.com>
2024-07-11 22:05:51 +0300

    Updated

--- src/context/PropertiesContext.jsx
+++ src/context/PropertiesContext.jsx
@@ -1,27 +1,104 @@
-import React, { useState } from 'react'
-import { createContext } from 'react'
-export const PropertyContext = React.createContext()
-export const PropertyProvider = ({children}) => {
+import React, { useState, useEffect } from 'react';
+import { createContext } from 'react';
 
+export const PropertyContext = createContext();
 
-    const [currentProperty, setCurrentProperty] = useState("")
+export const PropertyProvider = ({ children }) => {
+  const [properties, setProperties] = useState([]);
 
+  useEffect(() => {
+    fetchProperties();
+  }, []);
 
+  const fetchProperties = () => {
+    fetch('http://127.0.0.1:5000/real_estates')
+      .then((response) => response.json())
+      .then((res) => {
+        setProperties(res);
+      })
+      .catch((error) => {
+        console.error('Error fetching properties:', error);
+      });
+  };
 
-    const register_Property = () => {
-        setCurrentProperty()
-    }
+  const addProperty = (propertyData) => {
+    fetch(`http://127.0.0.1:5000/real_estates`, {
+      method: 'POST',
+      body: JSON.stringify(propertyData),
+      headers: {
+        'Content-type': 'application/json',
+        // 'Authorization': `Bearer ${auth_token}`
+      }
+    })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.success) {
+        toast.success(res.success);
+        fetchProperties(); // Refresh properties list
+      } else if (res.error) {
+        toast.error(res.error);
+      }
+    })
+    .catch((error) => {
+      console.error('Error adding property:', error);
+    });
+  };
 
-    const contextData = {
-        currentProperty, 
-        setCurrentProperty,
-        register_Property
-    }
+  const editProperty = (propertyId, updatedData) => {
+    fetch(`http://127.0.0.1:5000/real_estates/${propertyId}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedData),
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      }
+    })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.success) {
+        toast.success(res.success);
+        fetchProperties(); // Refresh properties list
+      } else if (res.error) {
+        toast.error(res.error);
+      }
+    })
+    .catch((error) => {
+      console.error('Error editing property:', error);
+    });
+  };
 
+  const deleteProperty = (propertyId) => {
+    fetch(`http://127.0.0.1:5000/real_estates/${propertyId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${auth_token}`
+      }
+    })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.success) {
+        toast.success(res.success);
+        fetchProperties(); // Refresh properties list
+      } else if (res.error) {
+        toast.error(res.error);
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting property:', error);
+    });
+  };
 
-    return (
-        <PropertyContext.Provider value={{contextData}}>
-            {children}
-        </PropertyContext.Provider>
-    )
-}
+  const contextData = {
+    properties,
+    addProperty,
+    editProperty,
+    deleteProperty
+  };
+
+  return (
+    <PropertyContext.Provider value={contextData}>
+      {children}
+    </PropertyContext.Provider>
+  );
+};
